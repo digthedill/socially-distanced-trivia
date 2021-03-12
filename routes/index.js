ff12c17d91da1835aef9e90a6e2fcec350b12631
digthedill <popssly@gmail.com>
2021-03-12 11:18:15 -0600

    ready to deploy

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const request = require("request");
-const router = express.Router();
+const express = require("express")
+const cors = require("cors")
+const request = require("request")
+const router = express.Router()
 
-router.use(cors());
+router.use(cors())
 router.get("/", (req, res) => {
   res
     .send({ response: "Welcome to the trivia api using socket.io and react" })
-    .status(200);
-});
+    .status(200)
+})
 
 router.get("/start", (req, res) => {
   request({
     uri: "https://opentdb.com/api.php?amount=10",
-  }).pipe(res);
-});
+  }).pipe(res)
+})
 
-module.exports = router;
-
-// store data for all clients....
-
-// client emit start
-//server makes the router request
-//server emits response to all clients in the game room
+module.exports = router
